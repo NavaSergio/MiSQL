7401b4e5ada653b05cba0102fde7827246320973
NavaSergio <s3rgio.nava@gmail.com>
2023-01-18 13:18:53 -0600

    ejercicios completos

--- paso.R
+++ paso.R
@@ -1,1 +1,12 @@
-company_mast <- read.delim("clipboard")
+orders <- read.delim("clipboard")
+
+
+
+library(readxl)
+salesman       <- read_excel("tablas.xlsx", sheet = "salesman")
+customer       <- read_excel("tablas.xlsx", sheet = "customer")
+orders         <- read_excel("tablas.xlsx", sheet = "orders")
+company_mast   <- read_excel("tablas.xlsx", sheet = "company_mast")
+item_mast      <- read_excel("tablas.xlsx", sheet = "item_mast")
+emp_department <- read_excel("tablas.xlsx", sheet = "emp_department")
+emp_details    <- read_excel("tablas.xlsx", sheet = "emp_details")
